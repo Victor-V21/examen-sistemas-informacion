b872f142d91c83f048dee39caf592203178fabbb
Victor Vega <vegav2605@gmail.com>
2025-11-27 12:29:33 -0600

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, View } from "react-native";
+import "../global.css";
+
+const index = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+};
+
+export default index;
